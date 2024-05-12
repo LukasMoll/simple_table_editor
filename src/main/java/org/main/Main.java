04a03d325dca752da049ca23922db0de5da0bcea
Lukas Molleman <Lukas.Molleman@gmail.com>
2024-05-12 08:44:43 +0200

    Fixed main args != 5 issue

--- src/main/java/org/main/Main.java
+++ src/main/java/org/main/Main.java
@@ -5,6 +5,10 @@ import javax.swing.*;
 
 public class Main {
     public static void main(String[] args) {
+        if (args.length != 5) {
+            System.out.println("Requires 5 arguments: <width> <height> <screenWidth> <screenHeight> <maxHistory>");
+            System.exit(1);
+        }
         int width = Integer.parseInt(args[0]);
         int height = Integer.parseInt(args[1]);
         int screenWidth = Integer.parseInt(args[2]);
